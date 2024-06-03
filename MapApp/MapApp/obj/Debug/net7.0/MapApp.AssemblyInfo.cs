1ad16d2bc622b0c186668604a2c7de71b423738d
Bastian Aron Kramer <bastian4444@live.dk>
2024-06-03 17:46:02 +0200

    jobs done

--- MapApp/MapApp/obj/Debug/net7.0/MapApp.AssemblyInfo.cs
+++ MapApp/MapApp/obj/Debug/net7.0/MapApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e7063b76b0f397b16115a993cb22f34d78d1235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72050a3acc350d1fdbc9f30f2b0ef9e6218788e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
